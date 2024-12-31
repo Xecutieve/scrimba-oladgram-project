b56f94b267b5003eee469aa738d7df7bf9072e1f
GorkeemC <p.gorkemcindemir@outlook.com>
2024-12-31 09:16:01 +0300

    Fix the problem with p elemenet not rendering properly.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/**
+ * An array of post objects representing social media posts.
+ * Each object contains information about a user's post, including their identity, location, and post details.
+ *
+ * @type {Array<Object>}
+ * @property {string} name - The full name of the user who created the post.
+ * @property {string} username - The username of the post's creator.
+ * @property {string} location - The geographical location of the user at the time of posting.
+ * @property {string} avatar - The file path to the user's avatar image.
+ * @property {string} post - The file path to the content image of the post.
+ * @property {string} comment - A comment or caption provided by the user for the post.
+ * @property {number} likes - The number of likes the post has received.
+ */
 const posts = [
     {
         name: "Vincent van Gogh",
@@ -28,6 +41,17 @@ const posts = [
     }
 ]
 
+/**
+ * Represents the DOM element with the ID "post-page".
+ * This variable is used to access and manipulate the corresponding
+ * HTML element within the document.
+ *
+ * The variable is typically used to perform operations such as
+ * modifying content, applying styles, or attaching event listeners
+ * to the specific element identified by the "post-page" ID.
+ *
+ * @type {HTMLElement | null}
+ */
 const postPage = document.getElementById("post-page")
 
 for(let i = 0; i < posts.length; i++) {
@@ -35,7 +59,7 @@ for(let i = 0; i < posts.length; i++) {
         <section>
                 <div class="container">
                     <div class="post-content">
-                        <img class="post-owner-avatar" src="${posts[i].avatar}">
+                        <img class="post-owner-avatar" src="${posts[i].avatar}" alt="post owner's profile picture">
                         <div class="post-owner-info">
                             <h1>${posts[i].name}</h1>
                             <p>${posts[i].location}</p>
@@ -46,9 +70,9 @@ for(let i = 0; i < posts.length; i++) {
                         <img src="${posts[i].post}" alt="van gogh's post">
                     </div>
                     <div class="icons">
-                        <img src="images/icon-heart.png">
-                        <img src="images/icon-comment.png">
-                        <img src="images/icon-dm.png">
+                        <img src="images/icon-heart.png" alt="like icon">
+                        <img src="images/icon-comment.png" alt="comment icon">
+                        <img src="images/icon-dm.png" alt="dm icon">
                     </div>
                     <div class="likes">
                         <h1>${posts[i].likes} likes</h1>
